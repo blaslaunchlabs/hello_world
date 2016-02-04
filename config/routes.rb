ab488b179ae4a9b0a46c4d0d19c318d218d6807d
blaslaunchlabs <blas_delossantos@launchlabs.ph>
2016-02-04 19:00:17 +0800

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
  
 
+  get 'sessions/new'
+
   get 'user/new'
 
   root          'static_pages#home'
@@ -9,6 +11,9 @@ Rails.application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   resources :users
 
